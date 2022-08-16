717925012ab68b4d7e3ce47eceab9666e747bd49
msidoranka <the_quiet_place@mail.ru>
2022-08-16 18:40:36 +0300

    feat: solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.from = min;
+        this.till = max;
     }
 
     guess() {
-
+        this.mid = Math.round((this.till + this.from) / 2);
+        return this.mid;
     }
 
     lower() {
-
+        this.till = this.mid;
     }
 
     greater() {
-
+        this.from = this.mid;
     }
 }
 
